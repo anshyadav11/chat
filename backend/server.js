dcccb9a20aefdd7f18c4c7a97a102fdd08cea114
Ansh <ansh.yadav.programming@gmail.com>
2024-12-13 00:58:41 +0530

    created auth routes and auth controllers

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,16 @@
-const express= require("express");
+import express from "express";
+import authRoutes from "./routes/authRoutes.js";
+import dotenv from "dotenv";
 
 const app=express();
+dotenv.config();
+const PORT =process.env.PORT || 5000;
+
 
 app.get("/",(req,res) => {
     res.send("hello world");
 })
-app.listen(5000,() => console.log('server rinning on port 5000'));
+
+app.use("/api/auth",authRoutes)
+
+app.listen(PORT,() => console.log('server rinning on port'+PORT));
